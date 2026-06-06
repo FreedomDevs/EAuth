d62faa97ef21b116f8628fbbb85a418a5b61fe78
mikinol <sm44aksdmiki13877kfh@gmail.com>
2026-06-06 14:39:28 +0300

    EAuth, ура

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+package dev.elysium.eauth
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+  override fun onEnable() {
+    logger.info("EAuth включен.")
+  }
+
+  override fun onDisable() {
+    logger.info("EAuth выключен.")
+  }
+}
