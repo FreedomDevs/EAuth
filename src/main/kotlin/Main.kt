869e1bbd8695e59ffece34a30f783434bbeab342
mikinol <sm44aksdmiki13877kfh@gmail.com>
2026-06-09 18:15:51 +0300

    Регистрация ивента

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,11 @@
 package dev.elysium.eauth
 
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
     override fun onEnable() {
+        Bukkit.getPluginManager().registerEvents(JoinDialogListener(), this)
         logger.info("EAuth включен.")
     }
 
